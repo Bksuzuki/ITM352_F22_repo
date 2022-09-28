4a58468fb938ae477fee9484a2ab96c1a57b5ab6
Bksuzuki <Bksuzuki@hawaii.edu>
2022-09-27 22:59:46 -1000

    push WOD

--- Lab7/Ex1.js
+++ Lab7/Ex1.js
@@ -1,8 +1,11 @@
 require("./products_data.js");
 var num_products = 5;
-var prod_num = 0;
-while (prod_num++ != num_products/2) {
-    console.log(($(prod_num).$(eval('name' + prod_num)))); 
-}
-console.log(`That's all we have!`)
- 
+//for (var prod_num=0; 
+ //   prod_num>num_products; 
+ //   prod_num++) {
+//console.log( `${prod_num}. ${eval('name' + prod_num )}`);}
+
+//console.log(`That's all we have!`);
+
+for (var prod_num = 1; eval("typeof name"+prod_num) != 'unidefined'; prod_num++) 
+    console.log(`${prod_num}. ${eval(`name` + prod_num)}`);
--- Lab7/Ex2.js
+++ Lab7/Ex2.js
@@ -1,12 +1,13 @@
+
 require("./products_data.js");
 var num_products = 5;
 var prod_num = 0;
-while (prod_num++ < num_products) {
-    console.log(prod_num); 
-    if((prod_num > num_products*0,.75) || (prod_num < num_products*.25)) {
-        console.log(`${eval('name' + prod_num)} is sold out!);
-        break;
+while(prod_num++ < num_products) {
+    
+    if( (prod_num > num_products*.75) || (prod_num < num_products*.25) ) {
+        console.log(`${eval('name' + prod_num )} is sold out!`);
+        continue;
     }
+    console.log(prod_num);
 }
-console.log(`That's all we have!`
  
